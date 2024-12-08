1556321990cfaf787b3e391976a27535bb422950
Jon Miller <jmiller@polly.co>
2024-12-08 11:18:41 -0500

    update login password length requirement

--- app/validators/login.ts
+++ app/validators/login.ts
@@ -23,8 +23,8 @@ export default function validateLogin(email: string, password: string) {
 
     if (!password) {
         errors.password = 'Password is required.';
-    } else if (password.length < 6) {
-        errors.password = 'Password must be at least 6 characters.';
+    } else if (password.length < 8) {
+        errors.password = 'Password must be at least 8 characters.';
     }
 
     return Object.keys(errors).length ? errors : null;
